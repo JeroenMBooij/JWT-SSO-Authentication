e9d8b7dbc3ae684b47ddeff60d14868dc924ff57
JeroenMBooij <49719495+JeroenMBooij@users.noreply.github.com>
2022-03-19 15:06:49 +0100

    add connection retry

--- Authentication.Persistance/DependencyInjection.cs
+++ Authentication.Persistance/DependencyInjection.cs
@@ -19,7 +19,7 @@ namespace Authentication.Persistance
             var connectionString = GetDatabaseConnectionString(configuration);
             services.AddDbContext<MainIdentityContext>(options =>
             {
-                options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+                options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString), providerOptions => providerOptions.EnableRetryOnFailure());
             });
 
             services.AddSingleton<IMainSqlDataAccess, MainSqlDataAccess>();
